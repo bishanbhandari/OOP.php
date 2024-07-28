28afcbef094390b5e51fea6928c6354aca1adf79
bishanbhandari <108720684+bishanbhandari@users.noreply.github.com>
2024-07-28 10:22:27 +0545

    Update person.php

--- learning OOP/person.php
+++ learning OOP/person.php
@@ -1,9 +1,19 @@
 <?php
 Class Person{
-    public $name="bishan";
-    public $email="bishanbhandari627@gmail.com";
-    public $password="text";
+    public $name;
+    public $email;
+    public $password;
+    public function __construct($name){
+        $this->name = $name;
+    }
 }
-$student =new person();
-echo $student->name;
+$student =new person("BISHAN");
+echo $student -> name;
+echo "<br>";
+$student1 =new person("XYZ");
+echo $student1->name;
+echo"<br>";
+$student2 =new person("ABC");
+echo $student2->name;
+
 ?>
