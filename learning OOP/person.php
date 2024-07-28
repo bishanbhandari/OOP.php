988a41e86182ddcc238d96e7f539e7f67ea9f333
bishanbhandari <108720684+bishanbhandari@users.noreply.github.com>
2024-07-28 10:01:33 +0545

    Add files via upload

---
+++ learning OOP/person.php
@@ -0,0 +1,9 @@
+<?php
+Class Person{
+    public $name="bishan";
+    public $email="bishanbhandari627@gmail.com";
+    public $password="text";
+}
+$student =new person();
+echo $student->name;
+?>
